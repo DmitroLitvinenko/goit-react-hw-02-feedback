5d6f010d1060f2f2fdc2c448c5e939b8058d5b5f
Dmytro Litvinenko <dima.litvinenko1990@gmail.com>
2023-04-19 00:30:44 +0300

    starting refactoring code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,29 @@
+import { Component } from 'react';
 import { Feedback } from './Feedback/Feedback';
 
-export const App = () => {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onAddFeedback = feedback => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Feedback
+          good={this.state.good}
+          bad={this.state.bad}
+          neutral={this.state.neutral}
+          onAddFeedback={this.onAddFeedback}
+        />
+      </>
+    );
+  }
+}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,61 +1,44 @@
-import React, { Component } from 'react';
 import { BtnBlock, Container } from './Feedback.styled';
 
-export class Feedback extends Component {
+export const Feedback = ({ good, neutral, bad, onAddFeedback }) => {
   //   static PropTypes = {};
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onAddFeedback = feedback => {
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     const total = good + neutral + bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     const total = good + neutral + bad;
-    const positivePercentage = Math.round((this.state.good / total) * 100);
+    const positivePercentage = Math.round((good / total) * 100);
     return positivePercentage;
   };
 
-  render() {
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <Container>
-        <h1>Please leave feedback</h1>
-        <BtnBlock>
-          <button type="button" onClick={() => this.onAddFeedback('good')}>
-            Good
-          </button>
-          <button type="button" onClick={() => this.onAddFeedback('neutral')}>
-            Neutral
-          </button>
-          <button type="button" onClick={() => this.onAddFeedback('bad')}>
-            Bad
-          </button>
-        </BtnBlock>
-        <h2>Statistics</h2>
-        <p>Good:{this.state.good}</p>
-        <p>Neutral:{this.state.neutral}</p>
-        <p>Bad:{this.state.bad}</p>
-        <p>Total:{total}</p>
-        <p>
-          Positive feedback:
-          {isNaN(positivePercentage) ? '0' : positivePercentage}%
-        </p>
-      </Container>
-    );
-  }
-}
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  return (
+    <Container>
+      <h1>Please leave feedback</h1>
+      <BtnBlock>
+        <button type="button" onClick={() => onAddFeedback('good')}>
+          Good
+        </button>
+        <button type="button" onClick={() => onAddFeedback('neutral')}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => onAddFeedback('bad')}>
+          Bad
+        </button>
+      </BtnBlock>
+      <h2>Statistics</h2>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad}</p>
+      <p>Total:{total}</p>
+      <p>
+        Positive feedback:
+        {isNaN(positivePercentage) ? '0' : positivePercentage}%
+      </p>
+    </Container>
+  );
+};
